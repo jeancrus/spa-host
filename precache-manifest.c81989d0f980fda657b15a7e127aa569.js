b82d3b98979a47dce86496b66d99754f9bfb8861
Jean Crus <jean.carlos@pucpr.edu.br>
2019-07-16 15:13:27 -0300

    Updates

---
+++ precache-manifest.c81989d0f980fda657b15a7e127aa569.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aed15dec9c59b8aa289a35287e26b85",
+    "url": "/spa-host/index.html"
+  },
+  {
+    "revision": "dc5099daddd5dfedcd30",
+    "url": "/spa-host/static/css/2.5fd700b8.chunk.css"
+  },
+  {
+    "revision": "44961614d00ef6503f35",
+    "url": "/spa-host/static/css/main.040ede79.chunk.css"
+  },
+  {
+    "revision": "dc5099daddd5dfedcd30",
+    "url": "/spa-host/static/js/2.d3f20189.chunk.js"
+  },
+  {
+    "revision": "44961614d00ef6503f35",
+    "url": "/spa-host/static/js/main.06664ad2.chunk.js"
+  },
+  {
+    "revision": "9d74494943e201de5796",
+    "url": "/spa-host/static/js/runtime~main.04e5f6a8.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/spa-host/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/spa-host/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/spa-host/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/spa-host/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/spa-host/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "6777486e7ac73f627f41dceddc0eedd1",
+    "url": "/spa-host/static/media/logo.6777486e.png"
+  }
+]);
